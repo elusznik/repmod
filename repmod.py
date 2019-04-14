8cb98c714b4b87b842ffb4d5abd380b6033e0fd5
elusznik <elusznik@gmail.com>
2019-04-14 23:22:37 +0200

    Sites added Read sites from .txt Placed regex-ready sites in a list Updated .gitignore

--- repmod.py
+++ repmod.py
@@ -6,9 +6,15 @@ def main():
     reddit = praw.Reddit("repmod")
     print("Authenticated as {}\n".format(reddit.user.me()))
     sub = reddit.subreddit("eluszniktest")
+    sites = []
+    with open("sites.txt","r",encoding="utf-8") as f:
+        for line in f:
+            sites.append('.' + line.strip().replace('.',r'\.') + '.')
+            print('.' + line.strip().replace('.',r'\.') + '.')
+    f.close()
 
     for item in sub.mod.spam():
-        print(item)
+        print(type(item))
 
 if __name__ == "__main__":
     main()
